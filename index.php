102f93de7c29d8314adda1b36dbc187e9250ee00
patriceBoyer <patriceboyer.44@gmail.com>
2020-08-29 22:57:43 +0200

    demarrage du projet MVC

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+if( isset($_GET['page']) && !empty($_GET['page'])){
+
+    $page = trim(strtolower($_GET['page']));
+} else {
+
+    $page = "home";
+}
+
+$allpages = scandir("controllers/");
+
+if(in_array($page.'controllers/',$allpages)){
+    include_once "models/" .$page. "_model.php";
+    include_once "views/" .$page. "_view.php";
+    include_once "controllers/" .$page. "_controller.php";
+    
+}
